35fde69a3ca0eab1dbaa199029cdddcc37842290
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-11-08 16:14:21 +0100

    project structure and Scanner importation

--- src/conversion.java
+++ src/conversion.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class conversion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Scanner 
+		Scanner sc = new Scanner(System.in);
+		
+		// ask with conversion user wants
+		// take a val
+			// if celsius convert to degree
+			// if degree convert to celsius
+			// if other then invalid
+		// display result
+		// ask if user is willing to make an other conversion
+	}
+	
+	// arrond a number
+	public static double arrondi (double A, int B) {
+		return (double) ((int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
 	}
 
 }
