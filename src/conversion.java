0fd4e68b6732acc66355b8cc4e713b800f9e02b3
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-11-08 16:50:18 +0100

    mode choice

--- src/conversion.java
+++ src/conversion.java
@@ -13,22 +13,27 @@ public class conversion {
 		System.out.println("TEMPARUTURE CONVERTOR");
 		System.out.println("=====================");
 		
-		
-		// ask with conversion user wants
-		System.out.println("Which conversion are you willing to make :");
-		System.out.println("1 - Celsius to Fahrenheit");
-		System.out.println("2 - Fahrenheit to Celsius");
-		mode = sc.nextLine().charAt(0);
-		
-		// if user choose a number but 1 or 2
-		if (mode != '1' || mode != '2') {
-		System.out.println("Wrong choice, you can only convert Celsius and Fahrenheit");
-		}
-		
+		do { // while user is willing to test temperature 
+			// ask which conversion user wants
+			mode = ' ';
+			System.out.println("Which conversion are you willing to make :");
+			System.out.println("1 - Celsius to Fahrenheit");
+			System.out.println("2 - Fahrenheit to Celsius");
+			mode = sc.nextLine().charAt(0);
+			
+			// if user choose a number but 1 or 2
+			if (mode != '1' && mode != '2') {
+			System.out.println("Wrong choice, you can only convert Celsius and Fahrenheit");
+			} while (mode != '1' && mode != '2');
+ 		
 		// take a val
 		System.out.println("Temperature to convert :");
-			// if celsius convert to degree
-			// if degree convert to celsius
+		toConvert = sc.nextDouble();
+		sc.nextLine(); // emptu line 
+			// if celsius convert to fahrenheit
+		if(mode = '1') {}
+			// if fahrenheit convert to celsius
+		if(mode = '2') {}
 			// if other then invalid
 		// display result
 		System.out.println(" °F refers to : " + " °C.");
